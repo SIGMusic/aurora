b55014277ecc49a8db3af3a3d3664683c8f515be
Alex Cordonnier <alexjcord@gmail.com>
2015-02-22 13:15:14 -0600

    Added manager

---
+++ manager/manager.py
@@ -0,0 +1,16 @@
+import bluetooth
+
+target_name = "Light"
+target_address = None
+
+nearby_devices = bluetooth.discover_devices()
+
+for addr in nearby_devices:
+    if bluetooth.lookup_name(addr).startswith("Light"):
+        target_address = addr
+        break
+
+if target_address is not None:
+    print "found target bluetooth device with address ", target_address
+else:
+    print "could not find target bluetooth device nearby"
